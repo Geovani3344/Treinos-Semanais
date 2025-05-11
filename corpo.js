1eacae9f3cf6e8ede40b4f550d864fdb138e9ca7
Geovani Rodrigues <geovanni3344@outlook.com>
2025-05-11 16:18:27 -0300

    troquei o fundo

--- corpo.js
+++ corpo.js
@@ -3,6 +3,7 @@ const treinos = {
   terca: { nome: "Superiores + Abdômen", exercicios: ["1 - Puxada frontal - 3x12", "2 - Remada baixa (Triângulo) - 3x12", "3 - Elevação lateral com halteres - 3x12", "4 - Desenvolvimento - 3x12", "5 - Rosca direta - 3x12", "6 - Rosca martelo - 3x12", "7 - Tríceps corda - 3x12", "8 - Tríceps barra - 3x12", "9 - Abdominal curto - 3x15", "Esteira - 20 minutos"] },
   quarta: { nome: "Quadríceps", exercicios: ["1 - Cadeira extensora - 4x12", "2 - Agachamento livre - 3x10", "3 - Leg press 45° - 3x12", "4 - Agachamento com halteres - 3x10", "5 - Cadeira adutora - 3x15", "6 - Panturrilha no leg press - 3x15", "7 - Panturrilha máquina - 3x20", "Esteira - 20 minutos"] },
   sexta: { nome: "Glúteos + Posterior de Coxa", exercicios: ["1 - Cadeira abdutora - 5x15", "2 - Coice na polia - 3x12", "3 - Mesa flexora - 4x12", "4 - Stiff - 3x12", "5 - Cadeira flexora - 3x10", "6 - Elevação Pelvica - 3x12", "7 - Panturrilha na máquina - 4x10", "Bike - 20 minutos"] },
+
 };
 
 function mostrarTreino(dia) {
